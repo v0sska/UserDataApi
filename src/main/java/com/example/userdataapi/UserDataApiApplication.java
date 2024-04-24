2475f38ab41891b9eaf6ecee47fa78cd3d5689bb
v0sska <speedatom80k@gmail.com>
2024-04-24 22:14:44 +0300

    Initial commit

---
+++ src/main/java/com/example/userdataapi/UserDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userdataapi/UserDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
