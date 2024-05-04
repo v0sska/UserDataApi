68aa8dd2ba9238e651f8629192d4683d9e31dbbb
v0sska <speedatom80k@gmail.com>
2024-05-04 18:00:16 +0300

    Create a pojo for transfer data to origin entity

--- src/main/java/com/example/userdataapi/interfaces/IBirthDateValidator.java
+++ src/main/java/com/example/userdataapi/interfaces/IBirthDateValidator.java
@@ -1,4 +1,12 @@
 package com.example.userdataapi.interfaces;
 
+import com.example.userdataapi.entities.Users;
+import java.time.LocalDate;
+
+
 public interface IBirthDateValidator {
+
+    boolean validateUserBirthDate(LocalDate birthDateToCheck);
+
+
 }
---
+++ src/main/java/com/example/userdataapi/pojos/UsersPojo.java
@@ -0,0 +1,4 @@
+package com.example.userdataapi.pojos;
+
+public class UsersPojo {
+}
