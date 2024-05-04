6e323f98e6e52a992ab6bc34b7ec7f726f9a43d4
v0sska <speedatom80k@gmail.com>
2024-05-04 21:42:08 +0300

    Created method for parsing adding data from multipart file

--- src/main/java/com/example/userdataapi/parsers/JsonUserDataParser.java
+++ src/main/java/com/example/userdataapi/parsers/JsonUserDataParser.java
@@ -3,10 +3,14 @@ package com.example.userdataapi.parsers;
 import com.example.userdataapi.interfaces.IBirthDateValidator;
 import com.example.userdataapi.interfaces.IUserDataParser;
 import com.example.userdataapi.pojos.UsersPojo;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component("jsonParser")
@@ -17,6 +21,28 @@ public class JsonUserDataParser implements IUserDataParser {
 
     @Override
     public List<UsersPojo> parseUserDataFromFile(MultipartFile fileToUpload) {
-        return null;
+
+        List<UsersPojo> uploadedPojo = new ArrayList<>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        try {
+            UsersPojo[] readPojo = objectMapper.readValue(fileToUpload.getInputStream(), UsersPojo[].class);
+
+            for(UsersPojo usersPojo : readPojo){
+                if(birthDateValidator.validateUserBirthDate(usersPojo.getBirthDate()))
+                    uploadedPojo.add(usersPojo);
+                else
+                    throw new IllegalArgumentException("User must be older 18!");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return uploadedPojo;
+
     }
 }
