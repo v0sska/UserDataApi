ca9a9353e69403ac9d642c157774f20a31c3074e
v0sska <speedatom80k@gmail.com>
2024-05-04 21:58:19 +0300

    create a method in controller for upload users data from multipart file

--- src/main/java/com/example/userdataapi/controllers/UserController.java
+++ src/main/java/com/example/userdataapi/controllers/UserController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -29,6 +30,15 @@ public class UserController {
         service.deleteById(id);
 
         return new ResponseEntity<>(Map.of("message", "User is deleted!"), HttpStatus.OK);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<Object> uploadUsersFromFile(@RequestParam("file") MultipartFile fileToUpload){
+
+        service.uploadUsersFromFile(fileToUpload);
+
+
+        return new ResponseEntity<>(Map.of("message", "Users is uploaded from file!"), HttpStatus.CREATED);
     }
 
 }
